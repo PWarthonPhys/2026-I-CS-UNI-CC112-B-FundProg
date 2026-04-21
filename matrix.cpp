ceb7c71ac40a9cf4f853d08f2c36dad49f474c44
PWarthonPhys <joaquinvelasquez04@gmail.com>
2026-04-21 17:33:48 -0500

    Ejercicio para arreglar matrices

--- matrix.cpp
+++ matrix.cpp
@@ -1,12 +1,45 @@
+
 #include "matrix.h"
 #include <iostream>
 
-
 using namespace std;
 
+void CreateMatrix(TP **&pMat, const size_t rows, const size_t cols){
+    pMat = new TP* [rows];
+
+    for (size_t i = 0; i < rows; ++i)
+        pMat[i] = new TP [cols];
+}
+
+void ReadMatrix(TP **pMat, const size_t rows, const size_t cols){
+    cout << "Ingrese " << rows*cols << " valores: ";
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            cin >> pMat[i][j];
+}
+
+
+
+void PrintMatrix(TP **pMat, const size_t rows, const size_t cols){
+    for (size_t i = 0; i < rows; ++i){
+        for (size_t j = 0; j < cols; ++j)
+            cout << pMat[i][j] << " ";
+        cout << endl;
+    }
+}
+
+
+void DeleteMatrix(TP **&pMat, const size_t rows){
+    for (size_t i = 0; i < rows; ++i)
+        delete[] pMat[i];
+    delete[] pMat;
+    pMat = nullptr;
+}
+
+//Matrizes
 void DemoMatrix1(){
     size_t rows, cols;
-    // TP **pMat = nullptr;
+    TP **pMat = nullptr;
 
     cout << "Demostracion de matrizes" << endl;
     cout << "Ingrese nro de filas: ";
@@ -14,8 +47,11 @@ void DemoMatrix1(){
     cout << "Ingrese nro de columnas: ";
     cin >> cols;
     
-    // CreateMatrix(pMat, rows, cols);
-    // ReadMatrix(pMat, rows, cols);
-    // PrintMatrix(pMat, rows, cols);
-    // DeleteMatrix(pMat, rows);
+    CreateMatrix(pMat, rows, cols);
+    ReadMatrix(pMat, rows, cols);
+
+    cout << "\nMatriz resultante:" << endl;
+    
+    PrintMatrix(pMat, rows, cols);
+    DeleteMatrix(pMat, rows);
 }
--- matrix.h
+++ matrix.h
@@ -1,9 +1,19 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
 
+#include <iostream>
 #include "types.h"
 
+using namespace std;
+
+void CreateMatrix(TP **&pMat, const size_t rows, const size_t cols);
+void ReadMatrix(TP **pMat, const size_t rows, const size_t cols);
+void PrintMatrix(TP **pMat, const size_t rows, const size_t cols);
+void DeleteMatrix(TP **&pMat, const size_t rows);
 
 void DemoMatrix1();
 
 #endif // __MATRIX_H__
+
+
+
